dad3ff02a56fb0505304056f142a7d271479495d
trikitrok <trikitrok@gmail.com>
2014-02-18 18:13:59 +0100

    Deleted obsolete imports

--- src/main/application/BirthdayService.java
+++ src/main/application/BirthdayService.java
@@ -2,13 +2,8 @@ package main.application;
 
 import java.util.List;
 
-import javax.mail.Message;
 import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
 import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
 
 import main.core.Employee;
 import main.core.EmployeesRepository;
