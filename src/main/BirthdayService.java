745bd2674a51442b4ceced18231e690e098efa5d
trikitrok <trikitrok@gmail.com>
2014-02-18 13:57:40 +0100

    Deleted all obsolete imports

--- src/main/BirthdayService.java
+++ src/main/BirthdayService.java
@@ -1,11 +1,5 @@
 package main;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.text.ParseException;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.mail.Message;
